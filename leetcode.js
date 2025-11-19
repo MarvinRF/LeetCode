2dbe7a6c87e8d493af8ec212d63824284c1a4510
MarvinRF <marvinvca@outlook.com>
2025-11-19 09:51:16 -0300

    staircase

---
+++ leetcode.js
@@ -0,0 +1,12 @@
+const n = 6;
+
+for (let i = 1; i <= n; i++) {
+  const spaces = " ".repeat(n - i);
+  const hashes = "#".repeat(i);
+  console.log(spaces + hashes);
+}
+
+//" ".repeat(n - i) custa O(n)
+//"#".repeat(i) também custa O(n)
+// O(n²) é inveitavel
+//Usa métodos nativos (repeat), que são otimizados internamente.
